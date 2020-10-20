7ccd9c77f5d85eadbfe853c46aa58a93be4ab802
王镇佳 <wangzhenjia@bytedance.com>
2020-10-21 00:36:03 +0800

    Updates

---
+++ precache-manifest.b7399b9b48339599768c4bc483f82cc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5343b8909819bcc4b8ef71b43d943cd3",
+    "url": "/data-handle-for-girlfriend/index.html"
+  },
+  {
+    "revision": "9a272beab16420905735",
+    "url": "/data-handle-for-girlfriend/static/css/2.cd846464.chunk.css"
+  },
+  {
+    "revision": "3bc16c074ab6992aae21",
+    "url": "/data-handle-for-girlfriend/static/css/main.c82fa903.chunk.css"
+  },
+  {
+    "revision": "9a272beab16420905735",
+    "url": "/data-handle-for-girlfriend/static/js/2.fc32c249.chunk.js"
+  },
+  {
+    "revision": "3d669e94f91837166de5dcb402c232d1",
+    "url": "/data-handle-for-girlfriend/static/js/2.fc32c249.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bc16c074ab6992aae21",
+    "url": "/data-handle-for-girlfriend/static/js/main.c5b43f14.chunk.js"
+  },
+  {
+    "revision": "e3ac459d3c54bd5b1ae2",
+    "url": "/data-handle-for-girlfriend/static/js/runtime-main.7fd197a6.js"
+  }
+]);
